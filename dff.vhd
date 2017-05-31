1677633f14c57bc8da70dfc60a644fa5e83c2d4d
George Katsaros <gkatsaros23@gmail.com>
2017-06-01 02:05:27 +0300

    adding component dff

---
+++ dff.vhd
@@ -0,0 +1,51 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    02:11:00 05/20/2017 
+-- Design Name: 
+-- Module Name:    dff - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.all;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity dff is
+port(
+clk : in std_logic;
+D : in signed (7 downto 0);
+Q :out signed (7 downto 0));
+end dff;
+
+architecture Behavioral of dff is
+
+begin
+process (clk)
+begin
+if (clk'event and clk='1') then
+Q<=D;
+end if;
+end process;
+
+end Behavioral;
+
